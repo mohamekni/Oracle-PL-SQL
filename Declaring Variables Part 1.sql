af4a76e6efe97a9a7c5f6afbacf110d0fac730d7
mohamekni <mekni.moha99@gmail.com>
2025-10-07 15:13:13 +0200

    Variables

--- Declaring Variables Part 1.sql
+++ Declaring Variables Part 1.sql
@@ -1,12 +1,21 @@
    set serveroutput on;
-
 declare
-   v_num1 number;
-   v_num2 number;
+   v_num1 number := &num1;
+   cnum2  number cosntant := &num2;
    v_num3 number;
 begin
-   v_num1 := 10;
-   v_num2 := v_num1 + 20;
-   v_num3 := v_num1 + v_num2;
+   v_num3 := v_num1 + c_num2;
    dbms_output.put_line(v_num3);
 end;
+/
+
+
+   set serveroutput on;
+declare
+   v_name varchar2(100);
+begin
+   dbms_output.put_line('Name: ' || v_name);
+   v_name := 'Moha';
+   dbms_output.put_line('Name: ' || v_name);
+end;
+/
--- Declaring Variables Part 3.sql
+++ Declaring Variables Part 3.sql
@@ -1,10 +1,32 @@
-declare
-   v_sal employees.salary%type;
+create or replace procedure get_salary (
+   v_id in employees.employee_id%type
+) is
+   declare v_sal number;
 begin
    select salary
      into v_sal
      from employees
-    where employee_id = &sal_id;
+    where employee_id = v_id;
    dbms_output.put_line('Salary: ' || v_sal);
 end;
 /
+
+
+
+   set serveroutput on;
+create or replace procedure isgood (
+   v_value number
+) is
+   istrue boolean;
+begin
+   istrue := v_value > 50;
+   if istrue is null then
+      dbms_output.put_line('v_value is NULL');
+   elsif istrue = true then
+      dbms_output.put_line('v_value is greater than 50');
+   elsif istrue = false then
+      dbms_output.put_line('v_value is less than or equal to 50');
+   end if;
+end;
+/
+EXECUTE isGood(100);
